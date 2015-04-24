0b077316c5b4cab30386b350d2427ebb7a6d4331
Svetogor <msvetogor@gmail.com>
2015-04-24 11:25:25 +0300

    init: angular-plurify.js

---
+++ angular-plurify.js
@@ -0,0 +1,32 @@
+angular.module('plurify', []).filter('plurify', [
+  function() {
+    return function(input, textOne, textFew, textMany) {
+      // input - число
+      // textOne - 'кот'
+      // textFew - 'кота'
+      // textMany - 'котов'
+
+      var modulo, plur;
+      plur = textOne;
+      modulo = input % 10;
+      if (input === 0) {
+        plur = textMany;
+      } else if (input === 1) {
+        plur = textOne;
+      } else if (input >= 2 && input <= 4) {
+        plur = textFew;
+      } else if (input >= 5 && input <= 19) {
+        plur = textMany;
+      } else if (modulo === 0) {
+        plur = textMany;
+      } else if (modulo === 1) {
+        plur = textOne;
+      } else if (modulo > 1 && modulo <= 4) {
+        plur = textFew;
+      } else {
+        plur = textMany;
+      }
+      return input + ' ' + plur;
+    };
+  }
+]);
